28f57f924918002e6d5836e15de0290f51173514
neoeahit <neoeahit@gmail.com>
2016-08-27 18:22:21 -0700

    integrating the UI and the backend

--- main.py
+++ main.py
@@ -1,9 +1,25 @@
-from flask import Flask
+#!/usr/bin/python
+#
+# Flask server, woo!
+#
+
+from flask import Flask, request, redirect, url_for, send_from_directory
+
+# Setup Flask app.
 app = Flask(__name__)
+app.debug = True
 
-@app.route("/")
-def hello():
-    return "Hello World!"
 
-if __name__ == "__main__":
-    app.run()
+# Routes
+@app.route('/')
+def root():
+  return app.send_static_file('index.html')
+
+@app.route('/<path:path>')
+def static_proxy(path):
+  # send_static_file will guess the correct MIME type
+  return app.send_static_file(path)
+
+
+if __name__ == '__main__':
+  app.run(port=7000)
---
+++ static/app/app.js
@@ -0,0 +1,167 @@
+
+angular.module('moneylending', ['facebook'])
+    .config(['$routeProvider', function($routeProvider){
+        $routeProvider
+            .when('/', {
+                templateUrl: '/app/views/home.html',
+                controller: ['$scope', function($scope){
+                    $scope.page = 'home';
+                }]
+            })
+            .when('/about', {
+                template: '<h2>{{page}}</h2>',
+                controller: ['$scope', function($scope){
+                    $scope.page = 'about';
+                }]
+            })
+            .otherwise({redirectTo: '/'});
+    }])
+    .config([
+    'FacebookProvider',
+    function(FacebookProvider) {
+     var myAppId = '100319957091588';
+     FacebookProvider.init(myAppId);
+     
+    }
+  ])
+  
+  .controller('MainController', [
+    '$scope',
+    '$timeout',
+    'Facebook',
+    function($scope, $timeout, Facebook) {
+      
+      // Define user empty data :/
+      $scope.user = {};
+      
+      // Defining user logged status
+      $scope.logged = false;
+      
+      // And some fancy flags to display messages upon user status change
+      $scope.byebye = false;
+      $scope.salutation = false;
+      
+      /**
+       * Watch for Facebook to be ready.
+       * There's also the event that could be used
+       */
+      $scope.$watch(
+        function() {
+          return Facebook.isReady();
+        },
+        function(newVal) {
+          if (newVal)
+            $scope.facebookReady = true;
+        }
+      );
+      
+      var userIsConnected = false;
+      
+      Facebook.getLoginStatus(function(response) {
+        if (response.status == 'connected') {
+          userIsConnected = true;
+        }
+      });
+      
+      /**
+       * IntentLogin
+       */
+      $scope.IntentLogin = function() {
+        if(!userIsConnected) {
+          $scope.login();
+        }
+      };
+      
+      /**
+       * Login
+       */
+       $scope.login = function() {
+         Facebook.login(function(response) {
+          if (response.status == 'connected') {
+            $scope.logged = true;
+            $scope.me();
+          }
+        
+        });
+       };
+       
+       /**
+        * me 
+        */
+        $scope.me = function() {
+          Facebook.api('/me?fields=id,name,email,picture', function(response) {
+          console.log(response.email)
+            /**
+             * Using $scope.$apply since this happens outside angular framework.
+             */
+            $scope.$apply(function() {
+              $scope.user = response;
+              console.log(response)
+            });
+            
+          });
+        };
+      
+      /**
+       * Logout
+       */
+      $scope.logout = function() {
+        Facebook.logout(function() {
+          $scope.$apply(function() {
+            $scope.user   = {};
+            $scope.logged = false;  
+          });
+        });
+      }
+      
+      /**
+       * Taking approach of Events :D
+       */
+      $scope.$on('Facebook:statusChange', function(ev, data) {
+        console.log('Status: ', data);
+        if (data.status == 'connected') {
+          $scope.$apply(function() {
+            $scope.salutation = true;
+            $scope.byebye     = false;    
+          });
+        } else {
+          $scope.$apply(function() {
+            $scope.salutation = false;
+            $scope.byebye     = true;
+            
+            // Dismiss byebye message after two seconds
+            $timeout(function() {
+              $scope.byebye = false;
+            }, 2000)
+          });
+        }
+        
+        
+      });
+      
+      
+    }
+  ])
+  
+  /**
+   * Just for debugging purposes.
+   * Shows objects in a pretty way
+   */
+  .directive('debug', function() {
+		return {
+			restrict:	'E',
+			scope: {
+				expression: '=val'
+			},
+			template:	'<pre>{{debug(expression)}}</pre>',
+			link:	function(scope) {
+				// pretty-prints
+				scope.debug = function(exp) {
+					return angular.toJson(exp, true);
+				};
+			}
+		}
+	})
+  
+  ;
+
